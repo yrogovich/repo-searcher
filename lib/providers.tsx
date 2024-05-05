bcf83a7f1abda53d1651439632d3f5eb7ed12539
Yaroslav Rogovich <yrogovich@gmail.com>
2024-05-05 17:18:44 +0200

    feat(providers): add react-query providers

---
+++ lib/providers.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import React, {useState} from 'react'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+export default function Providers({ children }: Readonly<{ children: React.ReactNode }>) {
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 6 * 10, // 10 minutes
+        refetchInterval: 1000 * 6 * 10,
+      },
+    },
+  }))
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      {children}
+    </QueryClientProvider>
+  );
+}
