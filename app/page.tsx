4d25a1758ead6d6e6206a670e09ad60659131ac1
Yaroslav Rogovich <yrogovich@gmail.com>
2024-05-05 17:55:03 +0200

    feat: add required Suspense for useSearchParams()

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import Container from "@/components/container"
 import { SearchForm } from "@/components/search-form"
 import SearchTable from "@/components/search-table"
+import { Suspense } from "react"
 
 export default function Home() {
   return (
     <main className="relative min-h-screen py-16">
       <Container className="space-y-6">
         <h1 className="text-2xl">Github Repos</h1>
-        <SearchForm />
-        <SearchTable />
+        <Suspense>
+          <SearchForm />
+          <SearchTable />
+        </Suspense>
       </Container>
     </main>
   )
