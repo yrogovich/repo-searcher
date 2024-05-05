fac5f32407cadb3a37407724200a7b376bf813f5
Yaroslav Rogovich <yrogovich@gmail.com>
2024-05-05 17:24:45 +0200

    feat(API): fetch search results from GitHub

--- server/actions/github-repos.ts
+++ server/actions/github-repos.ts
@@ -1,22 +1,34 @@
-import { Repo } from '@/app/columns';
+import { Octokit } from '@octokit/core';
+import { Repo } from '@/app/columns'
 
 type FetchReposResponse = {
   error?: string;
   data: Repo[];
 };
 
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
 export const fetchRepos = async (searchPhrase = ''): Promise<FetchReposResponse> => {
   if (!searchPhrase.trim()) return { data: [] };
 
-  return {
-    data: [
-      {
-        id: 1,
-        name: 'tonik',
-        owner: 'tonik',
-        stars: 1,
-        created_at: '2021-01-01',
-      }
-    ],
+  try {
+    const response = await octokit.request('GET /search/repositories', {
+      q: searchPhrase,
+    });
+
+    const items = response.data.items.map((item): Repo => ({
+      id: item.id,
+      name: item.name,
+      owner: item.owner?.login || 'Unknown owner',
+      stars: item.stargazers_count,
+      created_at: item.created_at,
+    }));
+
+    return { data: items };
+  } catch (error) {
+    console.error('Error fetching repositories:', error);
+    return { error: 'Failed to fetch data', data: []};
   }
 };
